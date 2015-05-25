f1693a0d90819f73381f7f4d9b500d92e1484e59
Cornellius Ngondo <devngondo@gmail.com>
2015-05-25 03:31:26 +0000

    Added jquery for the UI onhover

--- app/models/orphanage.rb
+++ app/models/orphanage.rb
@@ -2,6 +2,6 @@ class Orphanage < ActiveRecord::Base
   belongs_to :kid
   belongs_to :sponsor
 
-  has_attached_file :avatar, styles: { :medium => "300x300>" }
+  has_attached_file :avatar, styles: { :medium => "500x500>" }
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
