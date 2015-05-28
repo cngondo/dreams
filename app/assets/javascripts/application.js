5ad6294139d11b3bdf98225cce1980abb54ad2fa
Cornellius Ngondo <devngondo@gmail.com>
2015-05-28 07:12:41 +0000

    Errorhandling for jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,9 +9,10 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//
-//= require jquery
-//= require jquery_ujs
-//= require materialize-sprockets
-//= require turbolinks
-//= require_tree .
+//	
+// = require jquery
+// = require jquery_ujs
+// = require turbolinks
+// = require materialize-sprockets 
+// = require_tree   
+//  
